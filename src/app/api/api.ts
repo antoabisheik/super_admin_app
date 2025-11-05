26e7073abc9725ffcdb92a26433b16986ce5eb14
antoabisheik <Anto.a@smartan.ai>
2025-11-05 15:32:19 +0530

    Update API base URL to production endpoint

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,7 +1,7 @@
 // app/api/api.ts
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://192.168.0.102:8000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://webapps-middleware.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
