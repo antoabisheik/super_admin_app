a233af51ec977654548334f747ed2dc37852b5e9
Abicoc4j@ <Anto.a@smartan.ai>
2026-03-04 13:54:59 +0530

    nxt confg

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  turbopack: {
+    // Explicitly disable Turbopack
+    enabled: false,
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://smartanbackend.duckdns.org/api/:path*',
+      },
+    ];
+  },
+
+  webpack(config) {
+    return config;
+  },
+};
 
 export default nextConfig;
